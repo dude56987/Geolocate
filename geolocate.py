e7f37a124bab91f911864e8d5d2c82c86cdf84fb
dude56987 <dude56987@users.noreply.github.com>
2014-05-07 14:10:13 -0400

    Various fixes

--- geolocate.py
+++ geolocate.py
@@ -1,3 +1,21 @@
+#! /usr/bin/python
+########################################################################
+# Show rough location of current computer.
+# Copyright (C) 2014  Carl J Smith
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+########################################################################
 import urllib2, socket, sys
 def pullWebData(webAddress):
 	try:
@@ -30,5 +48,7 @@ output += 'lat ='+lat+'\n'
 output += 'lon ='+lon+'\n'
 if '--latlon' in sys.argv:
 	print lat+':'+lon
+elif '--lonlat' in sys.argv:
+	print lon+':'+lat
 else:
 	print output
